a1c19ef237822db4275f78353d962d294d2fd323
Jose Silva <joserota12@hotmail.com>
2020-09-20 21:03:07 +0200

    Profile por routerLink

--- src/app/clientes/profile/profile.component.ts
+++ src/app/clientes/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service'
 import swal from 'sweetalert2';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'profile-cliente',
@@ -14,6 +15,8 @@ export class ProfileComponent implements OnInit {
   cliente: Cliente;
   titulo: string = "Perfil del cliente"
   private imagenSeleccionada: File;
+  progreso: number = 0;
+
   constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -28,16 +31,34 @@ export class ProfileComponent implements OnInit {
     
   }
   seleccionarImagen(event){
+    
     this.imagenSeleccionada =  event.target.files[0];
+    this.progreso = 0;
     console.log(this.imagenSeleccionada) 
+    if(this.imagenSeleccionada.type.indexOf('image') < 0){
+      swal('Error al seleccionar imagen', 'El archivo debe ser una imagen', 'error')
+    }
   }
   subirImagen(){
+
+    if(!this.imagenSeleccionada){
+      swal('Error: Upload', 'Imagen no seleccionada', 'error')
+    }    if(this.imagenSeleccionada.type.indexOf('image') < 0){
+      swal('Error al seleccionar imagen', 'El archivo debe ser una imagen', 'error')
+    }else{
     
     this.clienteService.subirImagen(this.imagenSeleccionada, this.cliente.id)   
-    .subscribe(cliente => {
-      this.cliente = cliente;
-      swal('Imagen subida', `${cliente.imagen}`, 'success')
+    .subscribe(event => {
+      if(event.type === HttpEventType.UploadProgress){
+        this.progreso = Math.round(100 * event.loaded / event.total)
+      } else if(event.type == HttpEventType.Response){
+        let response:any = event.body;
+        this.cliente = response.cliente as Cliente;
+        swal('Imagen subida', response.mensaje, 'success')
+      }
+      
 
     })
   }
+}
 }
